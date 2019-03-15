35eb9e76eae888a0152ffdd7646e527677f58c86
fabiooliveira1 <fabioaurelio1269@gmail.com>
2019-03-15 13:12:14 -0300

    Add crypto log

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+const crypto = require('crypto')
+console.log('crypto', crypto)
+console.log('crypto.scryptSync', crypto.scryptSync)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
